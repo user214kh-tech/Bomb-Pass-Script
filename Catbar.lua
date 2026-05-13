1670fe0a177f357322106ac6036d22057141837c
user214kh-tech <user214kh@gmail.com>
2026-05-13 14:24:17 -0500

    Update Catbar.lua

--- Catbar.lua
+++ Catbar.lua
@@ -14,6 +14,7 @@ local _A5 = string.char(103,111,100,95,98,114,111,103,104,105) -- god_broghi
 local _A6 = string.char(85,115,101,114,95,116,104,117,110,103,116,104,117,110,103) -- "User_thungthung"
 local _A7 = string.char(65,89,85,66,86,83,65,89,85,66) -- "AYUBVSAYUB"
 local _A8 = string.char(79, 110, 108, 121, 65, 70, 114, 105, 101, 110, 100, 108, 121, 66, 97, 99, 111, 110) -- "OnlyAFriendlyBacon"
+local _A9 = string.char(85,115,101,114,95,98,97,99,107,117,112,50,49,52) -- "User_backup214"
 
 local AUTH_USERS = {   
     [_O] = true,   
@@ -24,7 +25,8 @@ local AUTH_USERS = {
     [_A5] = true,
     [_A6] = true,
     [_A7] = true,
-    [_A8] = true  -- Added onlyafriendlybacon
+    [_A8] = true,
+    [_A9] = true
 }
 
 
